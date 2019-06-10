5a5a1899895344242ea3a5f7d5067d804602c568
Howe <1013410795@qq.com>
2019-06-10 23:33:55 +0800

    add device-wise dataset

--- data_loader/data_sets.py
+++ data_loader/data_sets.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 from data_loader.transformer import *
 from data_manager.dcase19_lb_manager import Dcase19LBManager
+from data_manager.dcase19_eva_manager import Dcase19EVAManager
 from data_manager.dcase19_taskb import Dcase19TaskbData
 from data_manager.dcase19_standrizer import DCASE19Standarizer
 
@@ -116,3 +117,54 @@ class DevDataSet(Dataset):
             sample = self.transform(sample)
         return sample
 
+
+class EVA_Dataset(Dataset):
+    def __init__(self, is_divide_variance=True, transform=None):
+        super(EVA_Dataset, self).__init__()
+        self.data_manager = Dcase19EVAManager()
+        # load data, normed by all device data
+        self.x = self.data_manager.load_data()
+
+        dev_x, _ = load_dcase19_dev()
+
+        if is_divide_variance:
+            # normalize lb data using dev
+            from utils.utilities import standarize_with_norm
+            self.x = standarize_with_norm([self.x], dev_x)[0]
+        else:
+            from utils.utilities import mean_sub_norm
+            self.x = mean_sub_norm([self.x], dev_x)[0]
+
+        self.x = np.expand_dims(self.x, axis=1)
+
+        # self.x = np.repeat(self.x, repeats=3, axis=1)
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        return self.transform(self.x[idx])
+
+
+class DeviceWiseValSet(Dataset):
+    def __init__(self, is_divide_variance=True, device='a', transform=None):
+        super(DeviceWiseValSet, self).__init__()
+        self.dev_manager = Dcase19TaskbData()
+        dev_standrizer = DCASE19Standarizer(data_manager=self.dev_manager)
+        if is_divide_variance:
+            self.x, self.y = dev_standrizer.load_dev_standrized(mode='test', device=device, norm_device='abc')
+        else:
+            self.x, self.y = dev_standrizer.load_dev_mean_subtracted(mode='test', device=device, norm_device='abc')
+        self.x = np.expand_dims(self.x, axis=1)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, idx):
+        sample = (self.x[idx], self.y[idx])
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
