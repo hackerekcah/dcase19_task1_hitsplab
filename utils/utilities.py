2d931f236befcc3396add12000cec802e5efb885
hackerekcah <hackerekcah@outlook.com>
2019-06-10 15:28:59 +0800

    confusion matrix plot

--- utils/utilities.py
+++ utils/utilities.py
@@ -72,3 +72,88 @@ def set_logging(root_dir, args):
     for hdlr in log.handlers[:]:  # remove all old handlers
         log.removeHandler(hdlr)
     log.addHandler(fileh)  # set the new handler
+
+
+def calculate_confusion_matrix(target, predict, classes_num):
+    """Calculate confusion matrix.
+    Inputs:
+      target: integer array, (audios_num,)
+      predict: integer array, (audios_num,)
+      classes_num: int, number of classes
+    Outputs:
+      confusion_matrix: (classes_num, classes_num)
+    """
+
+    confusion_matrix = np.zeros((classes_num, classes_num))
+    samples_num = len(target)
+
+    for n in range(samples_num):
+        confusion_matrix[target[n], predict[n]] += 1
+
+    return confusion_matrix
+
+
+def plot_confusion_matrix(confusion_matrix, title, labels, values):
+    """Plot confusion matrix.
+    Inputs:
+      confusion_matrix: matrix, (classes_num, classes_num)
+      labels: list of labels
+      values: list of values to be shown in diagonal
+    Ouputs:
+      None
+    """
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+
+    cax = ax.matshow(confusion_matrix, cmap=plt.cm.Blues)
+
+    if labels:
+        ax.set_xticklabels([''] + labels, rotation=90, ha='left')
+        ax.set_yticklabels([''] + labels)
+        ax.xaxis.set_ticks_position('bottom')
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    for n in range(len(values)):
+        plt.text(n - 0.4, n, '{:.2f}'.format(values[n]), color='yellow')
+
+    plt.title(title)
+    plt.xlabel('Predicted')
+    plt.ylabel('Target')
+    plt.tight_layout()
+    plt.show()
+
+
+def calculate_accuracy(target, predict, classes_num, average=None):
+    """Calculate accuracy.
+    Inputs:
+      target: integer array, (audios_num,)
+      predict: integer array, (audios_num,)
+    Outputs:
+      accuracy: float
+    """
+
+    samples_num = len(target)
+
+    correctness = np.zeros(classes_num)
+    total = np.zeros(classes_num)
+
+    for n in range(samples_num):
+
+        total[target[n]] += 1
+
+        if target[n] == predict[n]:
+            correctness[target[n]] += 1
+
+    accuracy = correctness / total
+
+    if average is None:
+        return accuracy
+
+    elif average == 'macro':
+        return np.mean(accuracy)
+
+    else:
+        raise Exception('Incorrect average!')
