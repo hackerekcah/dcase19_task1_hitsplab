65bf668c543aabfdcf6e003e7a64474739c9c35e
Howe <1013410795@qq.com>
2019-06-11 18:26:15 +0800

    add multi-model function

--- class_wise_accuracy.py
+++ class_wise_accuracy.py
@@ -6,14 +6,16 @@ import numpy as np
 from utils.utilities import calculate_accuracy, plot_confusion_matrix, calculate_confusion_matrix
 
 
-def get_pred_prob(model, loader, device):
+def get_pred_prob(model, loader, device, using_softmax=False):
     model.eval()
     pred = []
     with torch.no_grad():
         for (x, y) in loader:
             x = x.to(device)
             batch_prob = model(x)
-
+            if using_softmax:
+                import torch.nn.functional as F
+                batch_prob = F.softmax(batch_prob, dim=1)
             batch_prob = batch_prob.cpu().detach().numpy()
             # move to cpu to release gpu prob
             pred.append(batch_prob)
@@ -62,10 +64,13 @@ def get_accuracy(args):
     val_target = get_target(val_loader)
 
     train_acc_class_wise = calculate_accuracy(target=train_target, predict=train_pred, classes_num=args.nb_class)
-    train_acc = np.mean(train_acc_class_wise)
+    # TODO more accurate
+    train_acc = (np.array(train_target) == np.array(train_pred)).mean()
+    # train_acc = np.mean(train_acc_class_wise)
 
     val_acc_class_wise = calculate_accuracy(target=val_target, predict=val_pred, classes_num=args.nb_class)
-    val_acc = np.mean(val_acc_class_wise)
+    val_acc = (np.array(val_target) == np.array(val_pred)).mean()
+    # val_acc = np.mean(val_acc_class_wise)
 
     labels = ['airport', 'bus', 'metro', 'metro_station', 'park', 'public_square',
               'shopping_mall', 'street_pedestrian', 'street_traffic', 'tram']
@@ -77,6 +82,69 @@ def get_accuracy(args):
         val_acc_cw[labels[i]] = val_acc_class_wise[i]
 
     return train_acc_cw, val_acc_cw, train_acc, val_acc
+
+
+def get_loaders(batch_size):
+    loaders = []
+    loaders.append(Device_Wise_Val_Loader(is_divide_variance=True).val(batch_size=batch_size))
+    loaders.append(Device_Wise_Medfilter_Val_Loader(is_divide_variance=False).val(batch_size=batch_size))
+    loaders.append(Device_Wise_MeanSub_Val_Loader(is_divide_variance=False).val(batch_size=batch_size))
+
+    return loaders
+
+
+def get_device_accuracy_multi_models(args):
+
+    # set up cuda device
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.device)
+    device = torch.device('cuda')
+
+    loaders = get_loaders(batch_size=args.batch_size)
+
+    from xception import ModifiedXception
+
+    labels = ['airport', 'bus', 'metro', 'metro_station', 'park', 'public_square',
+              'shopping_mall', 'street_pedestrian', 'street_traffic', 'tram']
+    val_acc_cw = []
+    val_acc_mean = []
+    val_acc_bc = []
+    for i in range(3):
+        model = ModifiedXception(num_classes=args.nb_class, drop_rate=args.drop_rate, decay=args.decay)
+        model.load_state_dict(torch.load(args.ckpt_file)['model_state_dict'])
+        model.to(device)
+        val_pred_prob1 = get_pred_prob(model, loaders[0][i], device)
+        model = ModifiedXception(num_classes=args.nb_class, drop_rate=args.drop_rate, decay=args.decay)
+        model.load_state_dict(torch.load(args.ckpt_file1)['model_state_dict'])
+        model.to(device)
+        val_pred_prob2 = get_pred_prob(model, loaders[1][i], device)
+        model = ModifiedXception(num_classes=args.nb_class, drop_rate=args.drop_rate, decay=args.decay)
+        model.load_state_dict(torch.load(args.ckpt_file2)['model_state_dict'])
+        model.to(device)
+        val_pred_prob3 = get_pred_prob(model, loaders[2][i], device)
+        val_pred_prob = val_pred_prob1 + val_pred_prob2 + val_pred_prob3
+        val_pred = np.argmax(val_pred_prob, axis=1)
+        val_target = get_target(loaders[0][i])
+
+        val_acc = calculate_accuracy(target=val_target, predict=val_pred, classes_num=args.nb_class)
+        if i > 0:
+            val_acc_bc.append(val_acc)
+        acc_mean = (np.array(val_target) == np.array(val_pred)).mean()
+        val_acc_mean.append(acc_mean)
+
+        val_acc_class_wise = dict()
+        for i in range(args.nb_class):
+            val_acc_class_wise[labels[i]] = val_acc[i]
+        val_acc_cw.append(val_acc_class_wise)
+
+    bc_val_acc = (val_acc_bc[1] + val_acc_bc[0]) / 2
+    val_acc_mean.append(np.mean(bc_val_acc))
+    val_acc_class_wise = dict()
+    for i in range(args.nb_class):
+        val_acc_class_wise[labels[i]] = bc_val_acc[i]
+    val_acc_cw.append(val_acc_class_wise)
+
+    return val_acc_cw, val_acc_mean
+
 
 
 def get_device_accuracy(args):
@@ -106,17 +174,33 @@ def get_device_accuracy(args):
               'shopping_mall', 'street_pedestrian', 'street_traffic', 'tram']
     val_acc_cw = []
     val_acc_mean = []
+    bc_val_pred = []
+    bc_val_target = []
     for i in range(3):
         val_pred_prob = get_pred_prob(model, val_loader[i], device)
         val_pred = np.argmax(val_pred_prob, axis=1)
         val_target = get_target(val_loader[i])
+        if i > 0:
+            bc_val_pred.append(val_pred)
+            bc_val_target.append(val_target)
         val_acc = calculate_accuracy(target=val_target, predict=val_pred, classes_num=args.nb_class)
-        val_acc_mean.append(np.mean(val_acc))
+        acc_mean = (np.array(val_target) == np.array(val_pred)).mean()
+        val_acc_mean.append(acc_mean)
+
         val_acc_class_wise = dict()
         for i in range(args.nb_class):
             val_acc_class_wise[labels[i]] = val_acc[i]
         val_acc_cw.append(val_acc_class_wise)
 
+    bc_val_target = np.concatenate(bc_val_target)
+    bc_val_pred = np.concatenate(bc_val_pred)
+    bc_val_acc = calculate_accuracy(target=bc_val_target, predict=bc_val_pred, classes_num=args.nb_class)
+    bc_val_acc_mean = (np.array(bc_val_target) == np.array(bc_val_pred)).mean()
+    val_acc_class_wise = dict()
+    for i in range(args.nb_class):
+        val_acc_class_wise[labels[i]] = bc_val_acc[i]
+    val_acc_cw.append(val_acc_class_wise)
+    val_acc_mean.append(bc_val_acc_mean)
     return val_acc_cw, val_acc_mean
 
 
@@ -124,18 +208,18 @@ def get_device_accuracy(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', default='2', type=str)
+    parser.add_argument('--device', default='3', type=str)
     parser.add_argument('--nb_class', default=10, type=int)
-    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--batch_size', default=64, type=int)
     parser.add_argument('--l2', default=0, type=float)
-    parser.add_argument('--decay', default=0.8, type=float)
+    parser.add_argument('--decay', default=1.0, type=float)
     parser.add_argument('--drop_rate', default=0.3, type=float)
     parser.add_argument('--method', default='normal', type=str)
     parser.add_argument('--ckpt_file2',
-                        default='ckpt/meansub_xcep_mixup/Run01,ModifiedXception,Epoch_35,acc_0.688319.tar',
+                        default='ckpt/meansub_xcep_mixup/Run01,ModifiedXception,Epoch_38,acc_0.691358.tar',
                         type=str)
     parser.add_argument('--ckpt_file1',
-                        default='ckpt/medfilter_xcep_mixup/Run01,ModifiedXception,Epoch_20,acc_0.712061.tar',
+                        default='ckpt/medfilter_xcep_mixup/Run01,ModifiedXception,Epoch_40,acc_0.718708.tar',
                         type=str)
     parser.add_argument('--ckpt_file',
                         default='ckpt/xcep_mixup/Run01,ModifiedXception,Epoch_38,acc_0.753086.tar',
@@ -144,27 +228,39 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # test of get_accuracy()
-    train_acc_cw, val_acc_cw, train_acc, val_acc = get_accuracy(args)
-    print('=' * 40)
-    print('Training set average accuracy: ', train_acc)
-    print('Training set class-wise accuray:')
-    for key in train_acc_cw.keys():
-        print('{:22s}: {:.3f}'.format(key, train_acc_cw[key]))
-    print('Validation set average accuracy: ', val_acc)
-    for key in val_acc_cw.keys():
-        print('{:22s}: {:.3f}'.format(key, val_acc_cw[key]))
-    print('=' * 40)
+    # train_acc_cw, val_acc_cw, train_acc, val_acc = get_accuracy(args)
+    # print('=' * 40)
+    # print('Training set average accuracy: ', train_acc)
+    # print('Training set class-wise accuray:')
+    # for key in train_acc_cw.keys():
+    #     print('{:22s}: {:.3f}'.format(key, train_acc_cw[key]))
+    # print('Validation set average accuracy: ', val_acc)
+    # for key in val_acc_cw.keys():
+    #     print('{:22s}: {:.3f}'.format(key, val_acc_cw[key]))
+    # print('=' * 40)
 
     # test of get_device_accuracy()
-    val_acc_cw, val_acc = get_device_accuracy(args)
-    device_list = ['device_A', 'device_B', 'device_C']
-    for i in range(3):
+    # val_acc_cw, val_acc = get_device_accuracy(args)
+    # device_list = ['device_A', 'device_B', 'device_C', 'Average (B, C)']
+    # for i in range(4):
+    #     print('{}:'.format(device_list[i]))
+    #     print('{} Validation set average accuracy: {:.3f}'.format(device_list[i], val_acc[i]))
+    #     print('{} Validation set class-wise accuracy:'.format(device_list[i]))
+    #     for key in val_acc_cw[i].keys():
+    #         print('{:22s}: {:.3f}'.format(key, val_acc_cw[i][key]))
+
+    # eva_predict(args)
+    # combine_predict(args)
+    # combine_predict3(args)
+
+    # ===================================================
+
+    val_acc_cw, val_acc = get_device_accuracy_multi_models(args)
+    # val_acc_cw, val_acc = get_device_accuracy(args)
+    device_list = ['device_A', 'device_B', 'device_C', 'Average (B, C)']
+    for i in range(4):
         print('{}:'.format(device_list[i]))
         print('{} Validation set average accuracy: {:.3f}'.format(device_list[i], val_acc[i]))
         print('{} Validation set class-wise accuracy:'.format(device_list[i]))
         for key in val_acc_cw[i].keys():
             print('{:22s}: {:.3f}'.format(key, val_acc_cw[i][key]))
-
-    # eva_predict(args)
-    # combine_predict(args)
-    # combine_predict3(args)
---
+++ main_lb_meansub_val_bc.py
@@ -0,0 +1,116 @@
+import argparse
+import os
+from data_loader.lb_loader import LB_MeanSub_Loader
+import torch.optim as optim
+from torch.optim.lr_scheduler import *
+from engine import *
+from utils.check_point import CheckPoint
+from utils.history import History, Reporter
+import logging
+from utils.utilities import set_seed, set_logging
+torch.backends.cudnn.benchmark = True
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def run(args):
+
+    set_seed(args.seed)
+
+    set_logging(ROOT_DIR, args)
+    logging.info(str(args))
+
+    # set up cuda device
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.device)
+    device = torch.device('cuda')
+
+    loader = LB_MeanSub_Loader(is_divide_variance=args.is_divide_variance)
+
+    train_loader = loader.train(batch_size=args.batch_size)
+    val_loader = loader.val(batch_size=args.batch_size)
+
+    # model = getattr(net_archs, args.net)(args).cuda()
+    from xception import ModifiedXception
+    model = ModifiedXception(num_classes=args.nb_class, drop_rate=args.drop_rate, decay=args.decay).cuda()
+
+    if args.optimizer == 'sgd':
+        optimizer = optim.SGD(model.parameters(), lr=args.init_lr, momentum=0.9, nesterov=True)
+    elif args.optimizer == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr=args.init_lr, weight_decay=args.l2)
+    if args.lr_factor < 1.0:
+        scheduler = ReduceLROnPlateau(optimizer, mode='max', verbose=True,
+                                      factor=args.lr_factor, patience=args.lr_patience)
+
+    train_hist, val_hist = History(name='train'), History(name='val')
+
+    if args.continue_run:
+        ckpt_file = Reporter(exp=args.exp).select_last(args.ckpt_prefix[0:5]).selected_ckpt
+        logging.info('continue training from {}'.format(ckpt_file))
+
+        ckpt_dicts = torch.load(ckpt_file)
+
+        model.load_state_dict(ckpt_dicts['model_state_dict'])
+        model.cuda()
+
+        optimizer.load_state_dict(ckpt_dicts['optimizer_state_dict'])
+
+        start_epoch = ckpt_dicts['epoch'] + 1
+    else:
+        start_epoch = 1
+
+    # checkpoint after new History, order matters
+    ckpter = CheckPoint(model=model, optimizer=optimizer, path='{}/ckpt/{}'.format(ROOT_DIR, args.exp),
+                        prefix=args.ckpt_prefix, interval=1, save_num=1)
+
+    for epoch in range(start_epoch, args.run_epochs):
+
+        train_mixup_all(train_loader, model, optimizer, device, mix_alpha=args.mix_alpha)
+
+        train_hist.add(
+            logs=eval_model(train_loader, model, device),
+            epoch=epoch
+        )
+        val_hist.add(
+            logs=eval_model(val_loader, model, device),
+            epoch=epoch
+        )
+        if args.lr_factor < 1.0:
+            scheduler.step(val_hist.recent['acc'])
+
+        # plotting
+        if args.plot:
+            train_hist.clc_plot()
+            val_hist.plot()
+
+        # logging
+        logging.info("Epoch{:04d},{:6},{}".format(epoch, train_hist.name, str(train_hist.recent)))
+        logging.info("Epoch{:04d},{:6},{}".format(epoch, val_hist.name, str(val_hist.recent)))
+
+        ckpter.check_on(epoch=epoch, monitor='acc', loss_acc=val_hist.recent)
+
+    # explicitly save last
+    ckpter.save(epoch=args.run_epochs-1, monitor='acc', loss_acc=val_hist.recent)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp', default='temp', type=str)
+    parser.add_argument('--net', default='ModifiedXception', type=str)
+    parser.add_argument('--ckpt_prefix', default='Run01', type=str)
+    parser.add_argument('--device', default='5', type=str)
+    parser.add_argument('--run_epochs', default=10, type=int)
+    parser.add_argument('--nb_class', default=10, type=int)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--l2', default=0, type=float)
+    parser.add_argument('--init_lr', default=3e-4, type=float)
+    parser.add_argument('--lr_patience', default=3, type=int)
+    parser.add_argument('--lr_factor', default=0.5, type=float)
+    parser.add_argument('--plot', default=False, type=bool)
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--optimizer', default='adam', type=str)
+    parser.add_argument('--decay', default=0.998, type=float)
+    parser.add_argument('--drop_rate', default=0.3, type=float)
+    parser.add_argument('--mix_alpha', default=0.1, type=float)
+    parser.add_argument('--continue_run', default=False, type=bool)
+    parser.add_argument('--is_divide_variance', default=True, type=bool)
+    args = parser.parse_args()
+    run(args)
